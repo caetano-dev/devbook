e8e3a72f32e7f05f0347025d25caacdca97e3115
drull1000 <pcaetanop@protonmail.com>
2021-12-13 18:58:24 -0300

    Styling login page

--- webapp/src/router/routes/routes.go
+++ webapp/src/router/routes/routes.go
@@ -20,5 +20,7 @@ func Config(router *mux.Router) *mux.Router {
 	for _, route := range routes {
 		router.HandleFunc(route.URI, route.Function).Methods(route.Method)
 	}
+	fileServer := http.FileServer(http.Dir("./assets/"))
+	router.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", fileServer))
 	return router
 }
