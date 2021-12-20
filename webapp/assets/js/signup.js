c52dcff8d5e4573ef0195dbddf4410dfdf8f512a
drull1000 <pcaetanop@protonmail.com>
2021-12-20 17:35:23 -0300

    Adding jquery

---
+++ webapp/assets/js/signup.js
@@ -0,0 +1,23 @@
+$('#signup-form').on('submit', createUser);
+
+function createUser(event){
+    event.preventDefault();
+
+    if ($('#password').val() != $('#confirm-password').val()){
+        alert('Passwords do not match');
+        console.log('createUser');
+        return;
+    }
+
+    $.ajax({
+        url: '/users',
+        method: 'POST',
+        data: {
+            name: $('#name').val(),
+            nick: $('#nick').val(),
+            email: $('#email').val(),
+            password: $('#password').val()
+        },
+    });
+
+}
